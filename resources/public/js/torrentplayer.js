359fedd36440972f0ab744ccfd2dfef6e30a2ffb
gmihai <mihai.giurgeanu@gmail.com>
2015-03-21 05:36:45 +0000

    Enhancements for html.

--- resources/public/js/torrentplayer.js
+++ resources/public/js/torrentplayer.js
@@ -1,52 +1,31 @@
 function torrentplayer(opts) {
-    var o = $.extend( {
-        mediaUrl: "/resources/streams"
-    }
-    ,
-    opts);
-    function showPalyer(mediaInfo) {
-        console.log("Starting player with mediaInfo: " + JSON.stringify(mediaInfo));
-        if(! o.element) {
-            throw "There is no element defined for the player.";
+    var o = $.extend({
+        mediaUrl: "/resources/streams",
+        onmediainfo: function(mediainfo) {
+            console.log("onmediainfo option not specified");
         }
-        //o.element.replaceWith("<video src='" + mediaInfo.url + "'></video>");
-        // var player = new MediaElementPlayer(o.element,
-        // {
-        //     enablePluginDebug: true, 
-        //     success: function(mediaElement) {
-        //         mediaElement.pause();
-        //         mediaElement.setSrc(mediaInfo.url);
-        //         mediaElement.play();
-        //     }
-        // }
-        // );
-        
-        o.element.attr("src", mediaInfo.url);
-        $('video,audio').mediaelementplayer({
-            enablePluginDebug: true
-        });
-    }
+    }, opts);
     return {
         createStream: function(magnetUrn) {
             var request = $.post(o.mediaUrl, magnetUrn);
-            request.done(function(mediaInfo, statusText, xhr) {
+            request.done(function(mediainfo, statusText, xhr) {
                 console.log("Response for createStream with status " + statusText);
-                if(xhr.status= 201) {
+                if(xhr.status == 201) {
                     console.log("Stream download created. New resource location: " + xhr.getResponseHeader('Location'));
                 }
-                showPalyer(mediaInfo);
-            }
-            );
-            request.fail(function (xhr,
-            statusText,
-            error) {
-                if(xhr.status= 409) {
+                console.log("Playing media " + mediainfo.url);
+                o.onmediainfo(mediainfo);
+            });
+            request.fail(function (xhr, statusText, error) {
+                if(xhr.status == 409) {
                     console.log("Stream download already exists." + xhr.getResponseHeader('Location'));
-                    showPalyer($.parseJSON(xhr.responseText));
+                    var mediainfo = JSON.parse(xhr.responseText);
+                    console.log("Playing media: " + mediainfo.url);
+                    o.onmediainfo(mediainfo);
+                } else {
+                    console.log("Error creating stream: " + statusText + "/" + error);
                 }
-                console.log("Error creating stream: " + statusText + "/" + error);
-            }
-            );
+            });
         }
     }
 }
